ae983351b7f6b281994f54164c4dc82bb0197914
“Sandeep <sandy.das11@gmail.com>
2019-09-21 19:22:42 +0530

    deck implementation

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { StyleSheet,View, Animated, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, Animated, TouchableOpacity } from 'react-native';
 import Ball from './src/Ball'
-export default class App extends Component {
-  render() {
+import Deck from './src/Deck'
+import { Card, Button } from 'react-native-elements'
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
+export default class App extends Component {
+
+  renderCard(item) {
+    return (
+      <Card
+      key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+
+      >
+        <Text style={{ marginBottom: 10 }}>{'I can customize the card further'}</Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor={'#03A9F4'}
+          title={'View now!'}
+        />
+      </Card>
+      // <Text>{item.text}</Text>
+    )
+  }
+
+  render() {
     return (
       <View style={styles.container} >
-        <Ball />
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+        />
       </View>
     );
   }
@@ -17,7 +52,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    
+
   },
- 
+
 });
